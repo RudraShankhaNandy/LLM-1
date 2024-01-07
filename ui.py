9f6b0cbade617e8fcce0bbccaf2405c149661552
RudraShankhaNandy <137567991+RudraShankhaNandy@users.noreply.github.com>
2024-01-07 23:54:52 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -2,6 +2,8 @@ import os
 import streamlit as st
 import requests
 from dotenv import load_dotenv
+from speech_to_text import listen_continuous
+from text_to_speech import synthesize_text
 
 # Load environment variables
 load_dotenv()
@@ -12,11 +14,13 @@ api_port = int(os.environ.get("PORT", 8080))
 # Streamlit UI elements
 st.title("Dropbox Search Tool")
 
+k = listen_continuous()
+
 question = st.text_input(
     "Search for something",
     placeholder="What data are looking for?"
 )
-
+question = k
 
 if question:
     url = f'http://{api_host}:{api_port}/'
@@ -27,5 +31,6 @@ if question:
     if response.status_code == 200:
         st.write("### Answer")
         st.write(response.json())
+        synthesize_text(response.json(),'output.mp3')
     else:
         st.error(f"Failed to send data to Pathway API. Status code: {response.status_code}")
